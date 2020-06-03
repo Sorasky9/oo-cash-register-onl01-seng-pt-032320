d4da67987c26bf1989fc6e44bc8f606903a5dd2d
Malcom Fetus <sora.vista@gmail.com>
2020-06-03 01:01:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
 attr_accessor :discount
 
 
-def initialize(total = 0, discount = nil)
+def initialize(total = 0, discount = 20)
   binding.pry
 @total = total
 if discount != nil
